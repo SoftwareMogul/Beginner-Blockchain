644a0fc6488ba337db60837a63db4c6eb46c0e25
SoftwareMogul <codewithdel@gmail.com>
2024-09-17 23:30:20 +0200

    Initialized Wallet Class

---
+++ backend/wallet/wallet.py
@@ -0,0 +1,13 @@
+import uuid
+
+from backend.config import STARTING_BALANCE
+
+class Wallet:
+    """
+    An individual wallet for a miner.
+    Keeps track of the miner's balance.
+    Allows a miner to authorize transactions
+    """
+    def __init__(self):
+        self.address = str(uuid.uuid4())[0:8]
+        
