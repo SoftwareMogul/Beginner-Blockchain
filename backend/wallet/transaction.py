5c02c4227ad8f2e9cbac4a69a5b53da21ba5e7d8
SoftwareMogul <codewithdel@gmail.com>
2024-09-23 22:45:51 +0200

    Added Transaction Validation & Wrote Tests

--- backend/wallet/transaction.py
+++ backend/wallet/transaction.py
@@ -58,6 +58,24 @@ class Transaction():
             self.output[sender_wallet.address] - amount
         
         self.input = self.create_input(sender_wallet, self.output)
+
+    @staticmethod
+    def is_valid_transaction(transaction):
+        """
+        Validate a transaction.
+        Raise an exception for invalid transactions.
+        """
+        output_total = sum(transaction.output.values())
+
+        if transaction.input['amount'] != output_total:
+            raise Exception('Invalid transaction output values')
+        
+        if not Wallet.verify(
+            transaction.input['public_key'],
+            transaction.output,
+            transaction.input['signature']
+        ):
+            raise Exception('Invalid signature')
         
         
 def main():
