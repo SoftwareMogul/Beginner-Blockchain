0b9587fb56f41115d866a0058dd623ae7e0abc5f
SoftwareMogul <codewithdel@gmail.com>
2024-10-06 14:17:38 +0300

    Finished Clear Transaction-Pool Transactions

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -27,9 +27,9 @@ def route_blockchain():
 @app.route('/blockchain/mine')
 def route_blockchain_mine():
     blockchain.add_block(transaction_pool.transaction_data())
-
     block = blockchain.chain[-1]
     pubsub.broadcast_block(block)
+    transaction_pool.clear_blockchain_transactions(blockchain)
 
     return jsonify(block.to_json())
 
