369433211ed7fc99c13111ab6a5f8c058cdefafc
SoftwareMogul <codewithdel@gmail.com>
2024-10-10 22:38:18 +0300

    Updated Tests

--- backend/tests/wallet/test_wallet.py
+++ backend/tests/wallet/test_wallet.py
@@ -29,3 +29,24 @@ def test_calculate_balance():
 
     assert Wallet.calculate_balance(blockchain, wallet.address) == \
         STARTING_BALANCE - amount
+
+    received_amount_1 = 25
+    received_transaction_1 = Transaction(
+        Wallet(),
+        wallet.address,
+        received_amount_1
+    )
+
+    received_amount_2 = 43
+    received_transaction_2 = Transaction(
+        Wallet(),
+        wallet.address,
+        received_amount_2
+    )
+
+    blockchain.add_block(
+        [received_transaction_1.to_json(), received_transaction_2.to_json()]
+    )
+
+    assert Wallet.calculate_balance(blockchain, wallet.address) == \
+        STARTING_BALANCE - amount + received_amount_1 + received_amount_2
